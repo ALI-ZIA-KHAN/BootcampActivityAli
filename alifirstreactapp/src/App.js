05e55e1378f2636550e25a27b033a37e41bf9b6a
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2020-10-06 14:25:29 +0500

    First react app try of class 2

---
+++ alifirstreactapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      This is my first try for react app!!!. So Again Hello World  By Ali.
+    </div>
+  );
+}
+
+export default App;
